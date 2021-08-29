3db061842617e2fad30ece86423400cd2cc2bd35
Denis112345 <40199504+Denis112345@users.noreply.github.com>
2021-08-29 23:18:16 +0500

    add_method_create_work
    Добавлен новый метод для создания работы

--- app/schema/schema.py
+++ app/schema/schema.py
@@ -1,5 +1,5 @@
 import graphene
-from graphene import ObjectType, String, Int, DateTime, List
+from graphene import ObjectType, String, Int, DateTime, List, Field
 from models import *
 
 class WorkForQuery(ObjectType):
@@ -11,12 +11,27 @@ class WorkForQuery(ObjectType):
 
 class Query(ObjectType):
     all_works = List(WorkForQuery, work_plan_id=Int())
+    create_work = Field(WorkForQuery, title_work=String(), date_start=DateTime(), date_end=DateTime(), work_plan_id=Int())
 
     def resolve_all_works(self, info, work_plan_id):
         work_plan = Work_plan.query.filter(Work_plan.id == work_plan_id).first()
         works = [WorkForQuery(title_work=w.title, work_plan_id=work_plan_id, date_start=w.date_start, date_end=w.date_end, work_id=w.id) for w in work_plan.works]
 
         return works
+
+    def resolve_create_work(self, info, title_work, date_start, date_end, work_plan_id):
+        work = Work(title=title_work, date_start=date_start, date_end=date_end)
+        work_plan = Work_plan.query.filter(Work_plan.id == work_plan_id).first_or_404() 
+        new_version_work_plan = Work_plan(title=work_plan.title, version=work_plan.version+1)
+        for w in [w for w in work_plan.works]:
+            new_version_work_plan.works.append(w)
+        new_version_work_plan.works.append(work)
+
+        db.session.add(work)
+        db.session.add(new_version_work_plan)
+        db.session.commit()
+
+        return "ok"
 
 shema = graphene.Schema(query=Query, auto_camelcase=False )
 
