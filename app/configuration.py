56d3119d1e5ec2a576afe1535a6107be82de3066
Denis112345 <40199504+Denis112345@users.noreply.github.com>
2021-09-02 18:43:10 +0500

    new_change

--- app/configuration.py
+++ app/configuration.py
@@ -1,7 +1,7 @@
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:roma.ru12@localhost/work_plan_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:roma.ru12@localhost/work_plan_db2'
     SECRET_KEY = 'roma.ru12'
 
     ### FLASK_SECURITY ###
--- app/schema/schema.py
+++ app/schema/schema.py
@@ -20,6 +20,7 @@ class Query(ObjectType):
     create_work = Field(WorkForQuery, title_work=String(), date_start=DateTime(), date_end=DateTime(), work_plan_id=Int())
     all_versions_for_work_plan = List(WorkPlanQuery, work_plan_title=String())
     delete_work = Field(WorkPlanQuery, id=ID(), work_plan_id=ID())
+    change_work = Field(WorkPlanQuery, id=ID(), work_plan_id=ID(), change=String())
 
     def resolve_all_works(self, info, work_plan_id):
         work_plan = Work_plan.query.filter(Work_plan.id == work_plan_id).first()
@@ -31,9 +32,10 @@ class Query(ObjectType):
         work = Work(title=title_work, date_start=date_start, date_end=date_end)
         work_plan = Work_plan.query.filter(Work_plan.id == work_plan_id).first_or_404() 
 
-        new_version_work_plan = Work_plan(title=work_plan.title+" "+str(work_plan.version+1), version=work_plan.version+1)
+        new_version_work_plan = Work_plan(title=work_plan.title[:-2]+" "+str(work_plan.version+1), version=work_plan.version+1, id=work_plan.id+1)
         for w in [w for w in work_plan.works]:
             new_version_work_plan.works.append(w)
+        print(work)
         new_version_work_plan.works.append(work)
 
         db.session.add(work)
@@ -42,8 +44,8 @@ class Query(ObjectType):
 
         return {"data": "ok"}
 
-    def resolve_all_versions_for_work_plan(self, info, work_plan_title):
-        work_plans = Work_plan.query.filter(Work_plan.title == work_plan_title).all()
+    def resolve_all_versions_for_work_plan(self, info, work_plan_id):
+        work_plans = Work_plan.query.filter(Work_plan.id == work_plan_id).all()
         return work_plans
 
     def resolve_delete_work(self, info, id, work_plan_id):
@@ -63,9 +65,43 @@ class Query(ObjectType):
             db.session.add(new_version_work_plan)
             db.session.commit()
 
-            return ''
+            return {"data": ["ok"]}
         except:
-            return ''
+            return {"data": ["Err"]}
+
+    def resolve_change_work(self, info, work_plan_id, id, change): # 'date_start: 2021-08-30 13:12:00'
+        work_plan = Work_plan.query.filter(Work_plan.id == work_plan_id).first()
+        work = [work for work in work_plan.works if work.id == int(id)][0]
+
+        params_for_change_work = change.split(',')
+        params_for_change_work = [par.split('=') for par in params_for_change_work]
+        new_work = Work(title=work.title, date_start=work.date_start, date_end=work.date_end, id=work.id+1)
+        for par in params_for_change_work:
+            if par[0] == 'date_start':
+                new_work.date_start = par[1]
+            elif par[0] == 'title':
+                new_work.title = par[1]
+            elif par[0] == 'date_end':
+                new_work.date_end = par[1]
+
+    
+        new_version_work_plan = Work_plan(title=work_plan.title[:-2]+" "+str(work_plan.version+1), version=work_plan.version+1, id=work_plan.id+1)
+        for w in [w for w in work_plan.works]:
+            if not w.id == work.id:
+                new_version_work_plan.works.append(w)
+
+        new_version_work_plan.works.append(new_work)
+
+        for w in new_version_work_plan.works:
+            print(w)
+        
+        db.session.add(new_work)
+        db.session.add(new_version_work_plan)
+        db.session.commit()
+        
+        return 'WorkPlanQuery'
+
+
         
 
 shema = graphene.Schema(query=Query, auto_camelcase=False )
